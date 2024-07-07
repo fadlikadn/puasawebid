23ad6a975da83f06b80d59efa8ec9fb9393b827f
Fadlika Dita Nurjanto <fadlikadn@gmail.com>
2024-07-08 00:26:05 +0700

    Update styling and use MonthView instead Calendar object

--- src/components/FullCalendar.tsx
+++ src/components/FullCalendar.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import Calendar from 'react-calendar'
+import { MonthView } from 'react-calendar'
 // import 'react-calendar/dist/Calendar.css'
 import './react-calendar-zendesk-garden.css'
 import { useState } from 'react'
@@ -17,11 +17,11 @@ const tileClassName = ({ date, view }: { date: Date, view: string }) => {
 }
 
 const FullCalendar = () => {
-    const [currentYear, setCurrentYear] = useState(year)
+    const [currentYear, _setCurrentYear] = useState(year)
     return (
-        <div>
+        <div className="bg-kale-100">
             <div className='flex justify-center mb-5'>
-                <h1 className="px-2 py-1 bg-yellow-200 round">Jadwal Puasa {currentYear}</h1>
+                <h1 className="px-2 py-1 font-semibold round">Jadwal Puasa {currentYear}</h1>
             </div>
             {/* Legend */}
             <div className="flex justify-center mb-4">
@@ -32,14 +32,25 @@ const FullCalendar = () => {
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                 {months.map((month, index) => (
                     <div key={index} className=''>
-                        <Calendar
+                        <h2 className='text-center font-semibold my-2'>{typeof month === 'string' ? month : month.toLocaleString('id-ID', { month: 'long' })}</h2>
+                        <MonthView 
+                            activeStartDate={new Date(currentYear, index)} 
+                            tileClassName={tileClassName} 
+                            valueType={'month'} 
+                            locale='id-ID'
+                        />
+                        {/* <Calendar
                             value={month}
                             tileClassName={tileClassName}
                             showNavigation={false}
                             locale='id-ID'
-                        />
+                        /> */}
                     </div>
                 ))}
+            </div>
+            {/* Open Source License*/}
+            <div className="text-center py-4">
+                <p>© {currentYear} Puasa.web.id. This project is open source and available under the MIT License.</p>
             </div>
         </div>
     )
