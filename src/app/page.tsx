3f4c716acab16da6d66bdfa40bd06458531d3b08
Fadlika Dita Nurjanto <fadlikadn@gmail.com>
2024-07-10 05:27:37 +0700

    remove container margin to fill color all page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import FullCalendar from "@/components/FullCalendar"
 const Home = () => {
     return (
         <Providers>
-            <div className="m-3 bg-kale-100 flex justify-center">
+            <div className="bg-kale-100 flex justify-center">
                 <FullCalendar />
             </div>
         </Providers>
